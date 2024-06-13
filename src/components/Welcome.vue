271e228e94395fb0dc4b8d72b60e73031b5dba04
Amanda Hansson <amanda9307@gmail.com>
2024-06-13 11:28:13 +0200

    rules + styling edits

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -3,13 +3,27 @@
 <template>
   <section>
     <h1>Tic Tac Toe</h1>
-    <img
-      src="../assets/tic-tac-toe.svg"
-      alt="two x and two o in white on a transparent background"
-      height="350"
-      width="350"
-    />
-    <button>Spela</button>
+    <div>
+      <img
+        src="../assets/tic-tac-toe.svg"
+        alt="two x and two o in white on a transparent background"
+        height="300"
+        width="300"
+      />
+      <div class="rules">
+        <p>
+          This is a two player game where players take turns placing their
+          symbol on an empty square.
+        </p>
+        <p>
+          The goal is to get three of your symbols in a row (horizontally,
+          vertically, or diagonally).If all squares are filled without a winner,
+          it's a draw.
+        </p>
+      </div>
+    </div>
+
+    <button>Play</button>
   </section>
 </template>
 
@@ -17,21 +31,43 @@
 section {
   display: grid;
   grid-template-columns: 1fr;
-  grid-template-rows: 7rem 420px 1fr;
+  grid-template-rows: 7rem 22rem 1fr;
   justify-items: center;
   align-items: center;
-  row-gap: 2rem;
+  row-gap: 1.5rem;
+}
+div {
+  display: grid;
+  grid-template-columns: 20rem 1fr;
+  gap: 2rem;
+  justify-items: center;
+  align-items: center;
 }
 h1 {
-  font-size: 6rem;
+  font-size: 9rem;
   margin-block: 0;
 }
 h1:hover {
-  font-size: 6.5rem;
+  font-size: 10rem;
+  color: rgb(187, 241, 190);
+}
+.rules {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+p {
+  width: 25rem;
+  text-align: left;
+  margin: 0;
+}
+img {
+  height: 18rem;
+  width: 18rem;
 }
 img:hover {
-  height: 400px;
-  width: 400px;
+  height: 20rem;
+  width: 20rem;
 }
 h1,
 img {
