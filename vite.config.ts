4ee19c67483e42aedf9340acbf1de28fc097bbae
Amanda Hansson <amanda9307@gmail.com>
2024-06-13 11:45:33 +0200

    deployment configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://medieinstitutet.github.io/vue-tictactoe-LilithSWE/", // Replace with your repository name
   plugins: [vue()],
-})
+});
